ca23fa552eb02f01b4776acb37db2f132fecf136
Kyrylo Shpytsya <kshpitsa@gmail.com>
2020-07-08 12:24:00 +0300

    Fix error in setup.py caused by missing comma

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         ],
     },
     classifiers=[
-        "Development Status :: 4 - Beta"
+        "Development Status :: 4 - Beta",
         # "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
